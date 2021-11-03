671661a8855157386e3332de03444b4bc64fa172
Domin1820 <domin1820@gmail.com>
2021-11-03 15:12:45 +0100

    add react filters

--- staff/domingo-ruiz/WebMobile-course3/practical-react/firebase-hosting-demo/src/App.js
+++ staff/domingo-ruiz/WebMobile-course3/practical-react/firebase-hosting-demo/src/App.js
@@ -1,44 +1,85 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
-
-const Scoreboard = () => {
-  const [score, setScore] = useState(0);
-}
+import './App.css'
+// import React from 'react';
 
 
-const App = () => {
-  const [schedule, setSchedule] = useState();
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+// const schedule = {
+//   "title": "CS Courses for 2018-2019",
+//   "courses": {
+//     "F101" : {
+//       "id" : "F101",
+//       "meets" : "MWF 11:00-11:50",
+//       "title" : "Computer Science: Concepts, Philosophy, and Connections"
+//     },
+//     "F110" : {
+//       "id" : "F110",
+//       "meets" : "MWF 10:00-10:50",
+//       "title" : "Intro Programming for non-majors"
+//     },
+//     "S313" : {
+//       "id" : "S313",
+//       "meets" : "TuTh 15:30-16:50",
+//       "title" : "Tangible Interaction Design and Learning"
+//     },
+//     "S314" : {
+//       "id" : "S314",
+//       "meets" : "TuTh 9:30-10:50",
+//       "title" : "Tech & Human Interaction"
+//     }
+//   }
+// };
 
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, []);
 
-  if (!schedule) return <h1>Loading schedule...</h1>;
+const Banner = props => (
+  <h1>{props.title}</h1>
+)
 
+// const CourseList = ({ courses }) => (
+//   <div className="course-list">
+//   { Object.values(courses).map(course => <Course course={ course } />) }
+//   </div>
+// );
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  
   return (
-    <div className="container">
-      <Banner title={ schedule.title } />
-      <CourseList courses={ schedule.courses } />
-    </div>
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      </div>
+   </>
   );
 };
 
-const Banner = ({ title }) => (
-  <h1>{ title }</h1>
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms).map(value => (
+      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+    ))
+  }
+  </div>
 );
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+Object.values(terms)
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
-);
+  ); 
 
 const getCourseNumber = course => (
   course.id.slice(1, 4)
@@ -53,12 +94,29 @@ const Course = ({ course }) => (
   </div>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
-  </div>
-);
 
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  } 
+  );
 
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
 export default App;
