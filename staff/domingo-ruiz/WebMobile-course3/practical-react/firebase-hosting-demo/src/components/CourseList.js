08ca2b70ac9133babd3c17ad9254aad135487ce0
Domin1820 <domin1820@gmail.com>
2021-11-09 10:50:11 +0100

    add task 2 running

---
+++ staff/domingo-ruiz/WebMobile-course3/practical-react/firebase-hosting-demo/src/components/CourseList.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import terms, { mapValues, addCourseTimes } from '../utilities/times.js';
+import { signInWithGoogle, signOut, useUserState } from '../utilities/firebase.js';
+import Course from './Course.js';
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const TermSelector = ({term, setTerm}) => {
+  const [user] = useUserState();
+  return (
+    <div className="btn-toolbar justify-content-between">
+      <div className="btn-group">
+      { 
+        Object.values(terms).map(
+          value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+        )
+      }
+      </div>
+      { user ? <SignOuButton /> : <SignInButton /> }
+    </div>
+  );
+};
+
+const SignInButton = () => (
+  <button className="btn btn-secondary btn-sm"
+      onClick={() => signInWithGoogle()}>
+    Sign In
+  </button>
+);
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const SignOuButton = () => (
+  <button className="btn btn-secondary btn-sm"
+      onClick={() => signOut()}>
+    Sign Out
+  </button>
+);
+
+const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] = useState([]);
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+    
+    return (
+      <> {}
+        <TermSelector term={term} setTerm={setTerm} />
+        <div className="course-list">
+        { 
+          termCourses.map(course =>
+            <Course key={ course.id } course={ course }
+              selected={selected} setSelected={ setSelected } 
+            />) 
+        }
+        </div>
+     </>
+    );
+  };
+
+export { addScheduleTimes, getCourseTerm };
+export default CourseList;
--- staff/domingo-ruiz/WebMobile-course3/practical-react/firebase-hosting-demo/src/utilities/times.js
+++ staff/domingo-ruiz/WebMobile-course3/practical-react/firebase-hosting-demo/src/utilities/times.js
@@ -1,56 +1,62 @@
-import { getCourseTerm } from '../components/courselist.js';
+import { getCourseTerm } from '../components/CourseList';
 
 const hasConflict = (course, selected) => (
-    selected.some(selection => courseConflict(course, selection))
-  );
+  selected.some(selection => courseConflict(course, selection))
+);
 
-  const mapValues = (fn, obj) => (
-    Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
-  );
-  
-  const addCourseTimes = course => ({
-    ...course,
-    ...timeParts(course.meets)
-  });
-  
-  export const addScheduleTimes = schedule => ({
-    title: schedule.title,
-    courses: mapValues(addCourseTimes, schedule.courses)
-  });
-  
-  
-  const hoursOverlap = (hours1, hours2) => (
-    Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
-  );
-  
-  
-  const days = ['M', 'Tu', 'W', 'Th', 'F'];
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
 
-  const daysOverlap = (days1, days2) => ( 
-    days.some(day => days1.includes(day) && days2.includes(day))
-  );
-  
-  const timeConflict = (course1, course2) => (
-    daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
-  );
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
 
-  const courseConflict = (course1, course2) => (
-    getCourseTerm(course1) === getCourseTerm(course2)
-    && timeConflict(course1, course2)
-  );
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
 
-  const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
-    const timeParts = meets => {
-    const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-    return !match ? {} : {
-    days,
-    hours: {
-        start: hh1 * 60 + mm1 * 1,
-        end: hh2 * 60 + mm2 * 1
+const courseConflict = (course1, course2) => (
+  getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+
+
+
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+return !match ? {} : {
+  days,
+  hours: {
+    start: hh1 * 60 + mm1 * 1,
+    end: hh2 * 60 + mm2 * 1
     }
-    };
+  };
 };
 
+const mapValues = (fn, obj) => (
+Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
 
-export {days, courseConflict, timeConflict};
-export default hasConflict;
+const addCourseTimes = course => ({
+...course,
+...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+title: schedule.title,
+courses: mapValues(addCourseTimes, schedule.courses)
+});
+
+
+
+export default terms;
+export { addScheduleTimes, addCourseTimes, mapValues, hasConflict, timeParts };
